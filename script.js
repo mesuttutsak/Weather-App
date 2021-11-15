73a46432b5cc86b424d0e4ea86ade6dff294b453
mesuttutsak <mesuttutsak.icmimar@gmail.com>
2021-11-15 19:32:53 +0300

    fist commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const url = 'https://openweathermap.org/data/2.5/'
+const key = 'e38d76e3c41152848c9539279ce326d2'
+
+const searchBar = document.getElementById("#search");
+searchBar.addEventListener('keypress', )
